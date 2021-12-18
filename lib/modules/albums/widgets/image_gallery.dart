ebca1740187d4274defc0524ca6b2ca51398fe95
Lex <gtagaev96@mail.ru>
2021-12-19 00:40:52 +0700

    Добавленно описание изображений. Обновлен SplashScreen

--- lib/modules/albums/widgets/image_gallery.dart
+++ lib/modules/albums/widgets/image_gallery.dart
@@ -5,41 +5,105 @@ import '../model/albums_model.dart';
 
 /// Виджет full screen image
 class ImageGalleryWidget extends StatefulWidget {
-  PageController pageController = PageController();
   final List<AlbumsModel> images;
   final int index;
-  ImageGalleryWidget({Key? key, required this.images, this.index = 0}) : pageController = PageController(initialPage: index);
+
+  const ImageGalleryWidget({Key? key, required this.images, this.index = 0})
+      : super(key: key);
 
   @override
   _ImageGalleryWidgetState createState() => _ImageGalleryWidgetState();
 }
 
 class _ImageGalleryWidgetState extends State<ImageGalleryWidget> {
+  ValueNotifier _data = ValueNotifier('');
+  PageController _pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: widget.index);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return PhotoViewGallery.builder(
-      scrollPhysics: const BouncingScrollPhysics(),
-      pageController: widget.pageController,
-      itemCount: widget.images.length,
-      builder: (context, index) {
-        final images = widget.images[index];
-        return PhotoViewGalleryPageOptions(
-            imageProvider: NetworkImage(images.url),
-          minScale: PhotoViewComputedScale.contained,
-          maxScale: PhotoViewComputedScale.contained * 4,
-        );
-      },
-      loadingBuilder: (context, event) => Center(
-        child: SizedBox(
-          width: 30.0,
-          height: 30.0,
-          child: CircularProgressIndicator(
-            value: event == null
-                ? 0
-                : event.cumulativeBytesLoaded / event.expectedTotalBytes!.toInt(),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          PhotoViewGallery.builder(
+            scrollPhysics: const BouncingScrollPhysics(),
+            pageController: _pageController,
+            itemCount: widget.images.length,
+            builder: (context, index) {
+              final images = widget.images[index];
+              _updateTitle(images.title);
+
+              return PhotoViewGalleryPageOptions.customChild(
+                minScale: PhotoViewComputedScale.contained,
+                maxScale: PhotoViewComputedScale.contained * 4,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(images.url),
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+                  ),
+                ),
+              );
+            },
+            loadingBuilder: (context, event) {
+              return Center(
+                child: SizedBox(
+                  width: 30.0,
+                  height: 30.0,
+                  child: CircularProgressIndicator(
+                    value: event == null
+                        ? 0
+                        : event.cumulativeBytesLoaded /
+                            event.expectedTotalBytes!.toInt(),
+                  ),
+                ),
+              );
+            },
           ),
-        ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: ValueListenableBuilder(
+                  valueListenable: _data,
+                  builder: (context, title, _) {
+                    return Text(
+                      _data.value.toString(),
+                      style:
+                          const TextStyle(color: Colors.white, fontSize: 24.0),
+                      maxLines: DefaultTextStyle.of(context).maxLines,
+                      overflow: TextOverflow.fade,
+                    );
+                  },
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
+  }
+
+  /// Обновление описания изображения
+  /// Принимает String [title]
+  void _updateTitle(String title) async {
+    await Future.delayed(
+      const Duration(microseconds: 1),
+    );
+    _data.value = title;
   }
 }
